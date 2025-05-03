42d613c549adfbea4cc503f1f5b8bec2e6144894
thiago <thiago_gja27@hotmail.com>
2025-05-03 19:37:16 -0300

    att

--- app/middleware.ts
+++ app/middleware.ts
@@ -5,18 +5,21 @@ import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
+
   const supabase = createMiddlewareClient({ req, res })
 
-  // Verifica a sessão usando o Supabase Auth
-  const { data: { session } } = await supabase.auth.getSession()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
 
-  // Redireciona para o login se não autenticado e tentar acessar '/dashboard'
-  if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
+  const isDashboard = req.nextUrl.pathname.startsWith('/dashboard')
+  const isLogin = req.nextUrl.pathname === '/login'
+
+  if (!user && isDashboard) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
 
-  // Redireciona para '/dashboard' se já estiver autenticado e tentar acessar '/login'
-  if (session && req.nextUrl.pathname === '/login') {
+  if (user && isLogin) {
     return NextResponse.redirect(new URL('/dashboard', req.url))
   }
 
